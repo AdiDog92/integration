230aa2b91e6fc01199a07c84f610772e48fcdb5a
AdiDog92 <adidog92@gmail.com>
2024-03-25 15:37:53 +0300

    Новые изменения

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 const showMore = document.querySelector(".our-stories__show-more");
 const videoInner = document.querySelector(".our-stories__vide-inner");
-const cardHeight = document.querySelector(".our-stories__card").clientHeight;
-const wrapperHeight = document.querySelector(".video-wrapper").clientHeight;
-let innerHeight = videoInner.clientHeight;
+let innerHeight = videoInner.offsetHeight;
 
 showMore.addEventListener("click", (e) => {
   e.preventDefault();
 
+  const cardHeight = document.querySelector(".our-stories__card").offsetHeight;
+  const wrapperHeight = document.querySelector(".video-wrapper").offsetHeight;
+
   innerHeight += cardHeight;
   videoInner.style.maxHeight = `${innerHeight + 24}px`;
 
-  console.log(innerHeight)
   if (videoInner.clientHeight >= wrapperHeight - cardHeight) {
-    document.querySelector(".our-stories__show-more").disabled = true;
-    videoInner.classList.add('end')
+    showMore.disabled = true;
+    videoInner.classList.add("end");
   }
 });
